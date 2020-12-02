3c2cec9f506fd98917b6d2adfc539bdd4ca4edf1
Public4 <public4@ya.ru>
2020-12-02 22:12:30 +0500

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App started at port ${PORT}...`);
+});
+
+app.get("/", (req, res, next) => {
+  res.send("Hello, CI!");
+});
